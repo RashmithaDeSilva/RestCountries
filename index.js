3da199d57d16654b09f2130cc85d6a3a9360b5df
Lahiru De Silva <lahirurashmitha00@gmail.com>
2025-02-18 13:49:57 +0530

    Change app module type

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express');
-require('dotenv').config();
+import express from 'express';
+import dotenv from 'dotenv';
+
+// Setup express app with port configs
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Run app
 app.use(express.json());
 app.listen(PORT, ()=>{
     console.log(`[INFO] - Server is running on port ${PORT}`);
