9c0ac4da9f04bf8ba48fa0167e5c0127ab2376ee
Lahiru De Silva <lahirurashmitha00@gmail.com>
2025-03-05 23:58:47 +0530

    Add standard response

--- src/routers/Router.mjs
+++ src/routers/Router.mjs
@@ -4,6 +4,7 @@ import StandardResponse from '../utils/StandardResponse.mjs';
 
 dotenv.config();
 const router = Router();
+const API_VERSION = process.env.API_VERSION;
 
 /**
  * @swagger
@@ -21,15 +22,20 @@ const router = Router();
  *               properties:
  *                 message:
  *                   type: string
- *                   example: "Welcome to the API. Use /api-docs for Swagger documentation."
+ *                   example: "Welcome to the API."
  */
 router.get('/', (req, res) => {
-    res.json({ message: `Welcome to the API. Use /api/${ process.env.API_VERSION }/api-docs for Swagger documentation (Only working on Developer mode).` });
+    let msg = `Welcome to the API, Use '/api/${ API_VERSION }/auth' to get authenticator for use this API.`;
+
+    if (process.env.ENV === "DEV") {
+        msg = `Welcome to the API, Use '/api/${ API_VERSION }/api-docs' for Swagger documentation (Only working on Developer mode).`;
+    }
+    res.status(200).send(StandardResponse(true, msg, null, null));
 });
 
 router.all("*", (req, res) => {
     return res.status(404).send(StandardResponse(false, "Not Found !", null, 
-        `Invalid endpoint, redirect to '/api/${ process.env.API_VERSION }/auth'`));
+        `Invalid endpoint, redirect to '/api/${ API_VERSION }/auth'`));
 });
 
 export default router;
--- src/utils/StandardResponse.mjs
+++ src/utils/StandardResponse.mjs
@@ -1,12 +1,10 @@
-class StandardResponse {
-    static newStandardResponse(status = false, messesge = null, data = null, error = null) {
-        return {
-            "status": status,
-            "messesge": messesge,
-            "data": data,
-            "error": error
-        };
+function StandardResponse(status = false, messesge = null, data = null, error = null) {
+    return {
+        "status": status,
+        "messesge": messesge,
+        "data": data,
+        "error": error
     };
-}
+};
 
 export default StandardResponse;
