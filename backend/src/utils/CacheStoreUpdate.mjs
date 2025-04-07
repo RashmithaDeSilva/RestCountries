702930ebb7a2a6a226d2285494a5e4139c8b788f
Lahiru De Silva <lahirurashmitha00@gmail.com>
2025-04-08 00:47:19 +0530

    Update temporarily method with cache store to fetch data from json file

--- backend/src/utils/CacheStoreUpdate.mjs
+++ backend/src/utils/CacheStoreUpdate.mjs
@@ -5,6 +5,7 @@ import CacheStoreModel from '../models/CacheStoreModel.mjs';
 import ErrorLogService from '../services/ErrorLogService.mjs';
 import { LogTypes } from './types/LogTypes.mjs';
 import { log } from './ConsoleLog.mjs';
+import { readFileSync } from 'fs';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -13,19 +14,23 @@ const errorLogService = new ErrorLogService();
 
 async function allCountries() {
     try {
-        // Fetch the data from the URL
-        const response = await fetch(process.env.DATA_RETRIEVE_API || "https://restcountries.com/v3.1/all");
-        if (!response.ok) {
-            throw new Error(CacheStoreErrors.FAILED_TO_FETCH_DATA + `(Response status: ${ response.statusText })`);
-        }
-        const data = await response.json();
-        log(LogTypes.INFO, "All countries data fetched successfully");
+        // // Fetch the data from the URL
+        // const response = await fetch(process.env.DATA_RETRIEVE_API || "https://restcountries.com/v3.1/all");
+        // if (!response.ok) {
+        //     throw new Error(CacheStoreErrors.FAILED_TO_FETCH_DATA + `(Response status: ${ response.statusText })`);
+        // }
+        // const data = await response.json();
+        // log(LogTypes.INFO, "All countries data fetched successfully");
+
+        // Load data from file
+        const fileData = readFileSync(process.env.DATA_RETRIEVE_FILE, 'utf-8')
+        const data = JSON.parse(fileData);
         
         await cacheStoreService.saveCache(new CacheStoreModel("cache:countries", data));
         log(LogTypes.INFO, "All countries cached save successfully");
 
     } catch (error) {
-        log(LogTypes.ERROR, "All countries fetching or caching data:");
+        log(LogTypes.ERROR, "All countries fetching or caching data");
         await errorLogService.createLog('CacheStoreController.allCountries', error);
     }
 }
